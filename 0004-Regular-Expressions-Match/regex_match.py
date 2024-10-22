95f34e1abc96d13b30125b6ad4946e16e3926307
oneananda <anandathirumurugan@gmail.com>
2024-10-22 18:45:49 +0530

    Print messages for re.match function

--- 0004-Regular-Expressions-Match/regex_match.py
+++ 0004-Regular-Expressions-Match/regex_match.py
@@ -8,17 +8,25 @@ It checks for matches at the beginning of the string.
 import re
 
 # Match at the beginning of the string
-print('Match at the beginning of the string')
+print('Match at the beginning of the string wity Hello')
 RESULT = re.match(r'Hello', 'Hello World')
 print(bool(RESULT))  # Output: True
 
 # No Match if not at the beginning of the string
-print('No Match if not at the beginning of the string')
+print('No Match if not at the beginning of the string with World')
 RESULT = re.match(r'World', 'Hello World')
 print(bool(RESULT))  # Output: False
 
 # Using ^ to Match the Start
 print('Using ^ to Match the Start')
+print('Checking if the word starts with H')
 RESULT = re.match(r'^H', 'Hello World')
 print(bool(RESULT))  # Output: True
 
+print('Checking if the word starts with He')
+RESULT = re.match(r'^He', 'Hello World')
+print(bool(RESULT))  # Output: True
+
+print('Checking if the word starts with Hi')
+RESULT = re.match(r'^Hi', 'Hello World')
+print(bool(RESULT))  # Output: False
