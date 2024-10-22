16d4bbca967d61ccdd274348e3893b1df7b9f4ee
oneananda <anandathirumurugan@gmail.com>
2024-10-22 18:42:09 +0530

    Using ^ to Match the Start

--- 0004-Regular-Expressions-Match/regex_match.py
+++ 0004-Regular-Expressions-Match/regex_match.py
@@ -8,9 +8,17 @@ It checks for matches at the beginning of the string.
 import re
 
 # Match at the beginning of the string
+print('Match at the beginning of the string')
 RESULT = re.match(r'Hello', 'Hello World')
 print(bool(RESULT))  # Output: True
 
 # No Match if not at the beginning of the string
+print('No Match if not at the beginning of the string')
 RESULT = re.match(r'World', 'Hello World')
 print(bool(RESULT))  # Output: False
+
+# Using ^ to Match the Start
+print('Using ^ to Match the Start')
+RESULT = re.match(r'^H', 'Hello World')
+print(bool(RESULT))  # Output: True
+
