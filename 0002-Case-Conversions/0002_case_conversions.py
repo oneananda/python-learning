25ae37b40cd253622cc0c553dfc8e30570868445
oneananda <anandathirumurugan@gmail.com>
2024-10-22 17:50:35 +0530

    Fix for Constant Name Not in UPPER_CASE (C0103)

--- 0002-Case-Conversions/0002_case_conversions.py
+++
@@ -1,9 +0,0 @@
-# 0002_case_conversions.py
-
-message = "Random message!"
-
-print("Original message: "+ message)
-
-print("Lower case: "+ message.lower())
-
-print("Upper case: "+ message.upper())
---
+++ 0002-Case-Conversions/case_conversions.py
@@ -0,0 +1,13 @@
+"""
+This module contains functions for performing case conversions on strings.
+"""
+
+# case_conversions.py
+
+MESSAGE = "Random message!"
+
+print("Original message: "+ MESSAGE)
+
+print("Lower case: "+ MESSAGE.lower())
+
+print("Upper case: "+ MESSAGE.upper())
