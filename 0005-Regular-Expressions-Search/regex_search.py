546ea3084aaf86f81be8c9ab801cdd70c6114a54
oneananda <anandathirumurugan@gmail.com>
2024-10-23 21:17:12 +0530

    searches for the word World! at the end of a different string

--- 0005-Regular-Expressions-Search/regex_search.py
+++ 0005-Regular-Expressions-Search/regex_search.py
@@ -11,7 +11,14 @@ print('Searches for the word World! at the end of the string')
 RESULT = re.search(r'World!$', 'Hello World!')
 print(bool(RESULT))  # Output: True
 
+print('Again searches for the word World! at the end of a different string')
+
 RESULT = re.search(r'World!$', 'Hello World Welcome!')
 print(bool(RESULT))  # Output: False
+
+print('Searches for the word World! everywhere')
+
+RESULT = re.search(r'World', 'Hello World Welcome!')
+print(bool(RESULT))  # Output: True
 
 
