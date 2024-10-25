a01f7a9c2e91957ef2f85a986949eeb8c9c7c764
oneananda <anandathirumurugan@gmail.com>
2024-10-25 21:29:41 +0530

    Adding Basic operations on array

--- 0007-Array-Functions/array_functions.py
+++ 0007-Array-Functions/array_functions.py
@@ -17,5 +17,11 @@ int_array.append(12)  # Append an element
 print("After appending:", int_array)
 
 # Removing elements
-int_array.remove(3)  # Remove an element
+int_array.remove(4)  # Remove an element
 print("After removing:", int_array)
+
+int_array = array.array('i', [2, 4, 6, 8, 10])
+
+# Remove element at index 2 (removes 6)
+int_array.pop(2)
+print("After removing element at index 2:", int_array)
