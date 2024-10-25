5910f2e5386d898df3eca201a5108b0b91fcb1a0
oneananda <anandathirumurugan@gmail.com>
2024-10-25 21:22:27 +0530

    Basic operations on array - Adding elements

--- 0007-Array-Functions/array_functions.py
+++ 0007-Array-Functions/array_functions.py
@@ -2,3 +2,17 @@
 This program demonstrates the use of the arrays in Python.
 """
 # array_functions.py
+
+import array
+
+# Create an integer array
+
+int_array = array.array('i', [2, 4, 6, 8, 10])
+print(int_array)
+
+# Basic operations on array
+
+# Adding elements
+int_array.append(12)  # Append an element
+
+print("After appending:", int_array)
