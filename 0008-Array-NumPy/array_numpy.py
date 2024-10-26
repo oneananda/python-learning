7116ff6d616c34e39bb7fef384c4ced8ba059c18
oneananda <anandathirumurugan@gmail.com>
2024-10-26 22:35:04 +0530

    NumPy arrays - Adding elements, Removing elements

--- 0008-Array-NumPy/array_numpy.py
+++ 0008-Array-NumPy/array_numpy.py
@@ -8,3 +8,11 @@ import numpy as np
 
 NUMPY_ARRAY = np.array([2, 4, 6, 8, 10])
 print("NumPy array:", NUMPY_ARRAY)
+
+# Adding elements - create a new array
+NEW_ARRAY = np.append(NUMPY_ARRAY, 6)
+print("After appending:", NEW_ARRAY)
+
+# Removing elements - create a new array without the specific element
+NEW_ARRAY = np.delete(NUMPY_ARRAY, 2)  # Deletes element at index 2 and add the result
+print("After removing:", NEW_ARRAY)
