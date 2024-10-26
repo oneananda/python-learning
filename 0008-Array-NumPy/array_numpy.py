aa6628db48cad24d5d2c11eb44283d716d02886b
oneananda <anandathirumurugan@gmail.com>
2024-10-26 18:25:31 +0530

    NumPy array

--- 0008-Array-NumPy/array_numpy.py
+++ 0008-Array-NumPy/array_numpy.py
@@ -6,5 +6,5 @@ This program demonstrates the use of the NumPy arrays in Python.
 
 import numpy as np
 
-NUM_ARRAY = np.array([2, 4, 6, 8, 10])
-print("NumPy array:", numpy_array)
+NUMPY_ARRAY = np.array([2, 4, 6, 8, 10])
+print("NumPy array:", NUMPY_ARRAY)
