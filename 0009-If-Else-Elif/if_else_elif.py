4664eae77b1755dcaeec5d2600611ee0bd1bb2af
oneananda <anandathirumurugan@gmail.com>
2024-10-27 15:49:55 +0530

    if-elif-else example

--- 0009-If-Else-Elif/if_else_elif.py
+++ 0009-If-Else-Elif/if_else_elif.py
@@ -6,8 +6,34 @@ This program demonstrates the use of the if_else_elif in Python.
 
 AGE = 40
 
+print(f"If-example")
+AGE = 18
+if AGE >= 18:
+    print("You are eligible to vote.")
+
+print(f"======================================================")
+
+
+print(f"If-else-example")
 print(f"Hospital admission when the age is {AGE}")
+
 if AGE <= 18:
     print("You should be admitted to the Pediatric Department.")
 else:
     print("You should be admitted to the Adult Department.")
+print(f"======================================================")
+
+print("if-elif-else example")
+
+print("elif = else if")
+
+MARKS = 85
+if MARKS >= 90:
+    print("Grade: A")
+elif MARKS >= 75:
+    print("Grade: B")
+elif MARKS >= 60:
+    print("Grade: C")
+else:
+    print("Grade: D")
+print(f"======================================================")
