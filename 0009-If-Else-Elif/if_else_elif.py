6c1a2da9acc3acf0a1b2417f5110ff1e1814715d
oneananda <anandathirumurugan@gmail.com>
2024-10-27 15:35:31 +0530

    if else - Hospital admission example

--- 0009-If-Else-Elif/if_else_elif.py
+++ 0009-If-Else-Elif/if_else_elif.py
@@ -3,3 +3,11 @@ This program demonstrates the use of the if_else_elif in Python.
 """
 
 # if_else_elif.py
+
+AGE = 40
+
+print(f"Hospital admission when the age is {AGE}")
+if AGE <= 18:
+    print("You should be admitted to the Pediatric Department.")
+else:
+    print("You should be admitted to the Adult Department.")
