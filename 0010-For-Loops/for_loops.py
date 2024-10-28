d0405c7933dcae430e22c1e3b2e401e8d7b1afda
oneananda <anandathirumurugan@gmail.com>
2024-10-28 19:22:01 +0530

    Adding for_loops.py

---
+++ 0010-For-Loops/for_loops.py
@@ -0,0 +1,17 @@
+"""
+This program demonstrates the use of the for loops in Python.
+"""
+
+# for_loops.py
+
+print("For loops example")
+print()
+print("Iterating Over a List")
+print()
+
+FRUITS = ["banana", "apple", "orange"]
+
+for fruit in FRUITS:
+    print(fruit)
+
+print("======================================================")
