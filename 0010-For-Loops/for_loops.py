21ed04776134a1508cf7e07ab4e443f5920b3583
oneananda <anandathirumurugan@gmail.com>
2024-10-28 19:32:07 +0530

    Formatting for comments

--- 0010-For-Loops/for_loops.py
+++ 0010-For-Loops/for_loops.py
@@ -7,8 +7,8 @@ This program demonstrates the use of the for loops in Python.
 print("For loops example")
 print()
 
-
-print("Iterating Over a List - start")
+COMMENT = "Iterating Over a List"
+print(f"{COMMENT} - start")
 print()
 
 FRUITS = ["banana", "apple", "orange"]
@@ -17,16 +17,29 @@ for fruit in FRUITS:
     print(fruit)
 
 print()
-print("Iterating Over a List - finish")
+print(f"{COMMENT} - finish")
 print("======================================================")
 
-
-print("Using range() to Iterate Over Numbers - start")
+COMMENT = "Using range() to Iterate Over Numbers"
+print(f"{COMMENT} - start")
 print()
 
 for i in range(10):
     print(i) # Print 10 numbers starting from 0 (0-9)
 
 print()
-print("Using range() to Iterate Over Numbers - finish")
+print(f"{COMMENT} - finish")
 print("======================================================")
+
+COMMENT = "Iterating Over a String"
+
+print(f"{COMMENT} - start")
+print()
+
+for i in range(10):
+    print(i) # Print 10 numbers starting from 0 (0-9)
+
+print()
+print(f"{COMMENT} - finish")
+print("======================================================")
+
