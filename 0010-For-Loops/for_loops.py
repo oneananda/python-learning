98cdf9e12bd2bd76f03dd77310cc1dd901dd1e74
oneananda <anandathirumurugan@gmail.com>
2024-10-28 19:26:14 +0530

    Using range() to Iterate Over Numbers

--- 0010-For-Loops/for_loops.py
+++ 0010-For-Loops/for_loops.py
@@ -6,7 +6,9 @@ This program demonstrates the use of the for loops in Python.
 
 print("For loops example")
 print()
-print("Iterating Over a List")
+
+
+print("Iterating Over a List - start")
 print()
 
 FRUITS = ["banana", "apple", "orange"]
@@ -14,4 +16,17 @@ FRUITS = ["banana", "apple", "orange"]
 for fruit in FRUITS:
     print(fruit)
 
+print()
+print("Iterating Over a List - finish")
+print("======================================================")
+
+
+print("Using range() to Iterate Over Numbers - start")
+print()
+
+for i in range(10): 
+    print(i) # Print 10 numbers starting from 0 (0-9)
+
+print()
+print("Using range() to Iterate Over Numbers - finish")
 print("======================================================")
